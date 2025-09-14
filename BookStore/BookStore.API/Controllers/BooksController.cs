118aea4a78619e5e2a1a08e98b752683af83b3e8
Kristaps Kozlovskis <228943118+techkristaps@users.noreply.github.com>
2025-09-14 10:16:27 +0300

    Added code

---
+++ BookStore/BookStore.API/Controllers/BooksController.cs
@@ -0,0 +1,164 @@
+﻿using BookStore.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.API.Controllers
+{
+    [Route("api/authors/{authorId}/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<BookDto>> GetBooks(int authorId)
+        {
+            var author = AuthorsDataStore.Current.Authors.FirstOrDefault(x => x.Id == authorId);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(author.Books);
+        }
+
+        [HttpGet("{bookid}", Name = "GetBook")]
+        public ActionResult<BookDto> GetBook(int authorId, int bookId)
+        {
+            // find an author first
+            var author = AuthorsDataStore.Current.Authors.FirstOrDefault(x => x.Id == authorId);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            // find a book
+            var book = author.Books.FirstOrDefault(x => x.Id == bookId);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult<BookDto> CreateBook(int authorId, [FromBody] BookForCreationDto book)
+        {
+            // find an author first
+            var author = AuthorsDataStore.Current.Authors.FirstOrDefault(x => x.Id == authorId);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            // demo purposes
+            var maxBooks = AuthorsDataStore.Current.Authors.SelectMany(x => x.Books).Max(b => b.Id);
+
+            var finalBook = new BookDto()
+            {
+                Id = ++maxBooks,
+                Title = book.Title
+            };
+
+            author.Books.Add(finalBook);
+
+            return CreatedAtRoute("GetBook", new
+            {
+                authorId = authorId,
+                BookId = finalBook.Id
+            }, finalBook);
+        }
+
+        [HttpPut("{bookid}")]
+        public ActionResult UpdateBook([FromRoute] int authorId, [FromRoute] int bookId, [FromBody] BookForUpdateDto book)
+        {
+            // find an author first
+            var author = AuthorsDataStore.Current.Authors.FirstOrDefault(x => x.Id == authorId);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            // find a book
+            var bookFromStore = author.Books.FirstOrDefault(x => x.Id == bookId);
+
+            if (bookFromStore == null)
+            {
+                return NotFound();
+            }
+
+            bookFromStore.Title = book.Title;
+
+            return NoContent();
+        }
+
+        [HttpPatch("{bookid}")]
+        public ActionResult PartiallyUpdateBook(int authorId, int bookId, JsonPatchDocument<BookForUpdateDto> patchDocument)
+        {
+            // find an author first
+            var author = AuthorsDataStore.Current.Authors.FirstOrDefault(x => x.Id == authorId);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            // find a book
+            var bookFromStore = author.Books.FirstOrDefault(x => x.Id == bookId);
+
+            if (bookFromStore == null)
+            {
+                return NotFound();
+            }
+
+            var bookToPatch = new BookForUpdateDto()
+            {
+                Title = bookFromStore.Title
+            };
+
+            patchDocument.ApplyTo(bookToPatch, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (!TryValidateModel(bookToPatch))
+            {
+                return BadRequest(ModelState);
+            }
+
+            bookFromStore.Title = bookToPatch.Title;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{bookid}")]
+        public ActionResult DeleteBook(int authorId, int bookId)
+        {
+            // find an author first
+            var author = AuthorsDataStore.Current.Authors.FirstOrDefault(x => x.Id == authorId);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            // find a book
+            var bookFromStore = author.Books.FirstOrDefault(x => x.Id == bookId);
+
+            if (bookFromStore == null)
+            {
+                return NotFound();
+            }
+
+            author.Books.Remove(bookFromStore);
+            return NoContent();
+        }
+    }
+}
---
+++ BookStore/BookStore.API/Models/AuthorDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStore.API.Models
+{
+    public class AuthorDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<BookDto> Books { get; set; } = new List<BookDto>();
+    }
+}
